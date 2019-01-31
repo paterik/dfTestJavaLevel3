14e3a1452f265b1a32f57a3fe2ccc52a0eb60931
Patrick Paechnatz <patrick.paechnatz@t-systems-mms.com>
2019-01-31 07:59:28 +0100

    doc: add simple link to math base

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
 
+    //
+    // https://www.frustfrei-lernen.de/mathematik/pascalsches-dreieck.html
+    //
+
     private static int pascal(int row, int column) {
 
         return 0;
