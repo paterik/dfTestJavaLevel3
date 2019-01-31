7de748e31f06b87a9432f1bc16d283d57316fdf2
Patrick Paechnatz <patrick.paechnatz@t-systems-mms.com>
2019-01-31 07:57:56 +0100

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    private static int pascal(int row, int column) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+        int max = 10;
+    }
+}
